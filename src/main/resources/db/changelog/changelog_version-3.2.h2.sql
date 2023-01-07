72ce7e7031698c3be651b53f98a189284e42be46
mat6plus <mat6plus@gmail.com>
2023-01-07 03:14:41 +0100

    price adjustment

--- src/main/resources/db/changelog/changelog_version-3.2.h2.sql
+++ src/main/resources/db/changelog/changelog_version-3.2.h2.sql
@@ -5,9 +5,9 @@ CREATE TABLE SALES (ID BIGINT AUTO_INCREMENT, ITEM VARCHAR(255), QUANTITY NUMBER
 --rollback DROP TABLE SALES;
 
 --changeset JohnD:15824-2
-INSERT INTO SALES (ITEM, QUANTITY, AMOUNT) VALUES ('Leather sofa By Michellotti', 4, 5026.63);
+INSERT INTO SALES (ITEM, QUANTITY, AMOUNT) VALUES ('Leather sofa By Michellotti', 4, 4026.63);
 --rollback DELETE FROM SALES WHERE ITEM='Leather sofa By Michellotti';
 
 --changeset JohnD:1582471835439-6
-INSERT INTO SALES (ITEM, QUANTITY, AMOUNT) VALUES ('Round coffee table By Michellotti', 1, 800.23);
+INSERT INTO SALES (ITEM, QUANTITY, AMOUNT) VALUES ('Round coffee table By Michellotti', 1, 900.23);
 --rollback DELETE FROM SALES WHERE ITEM='Round coffee table By Michellotti';
